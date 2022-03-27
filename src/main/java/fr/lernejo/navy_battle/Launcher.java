3df76848ba7a625db6e807759758255f7fc8c92f
Melinda Marmot <melinda.marmot@epita.fr>
2022-03-27 15:09:44 +0200

    HTTP Server (part 2)

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,15 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Launcher {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/ping", new PingHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 }
