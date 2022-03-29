3b451f6025e1a87cae77dc053ed1a76282aca645
Melinda Marmot <melinda.marmot@epita.fr>
2022-03-30 01:16:30 +0200

    Normally api/game/fire done

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -3,17 +3,21 @@ package fr.lernejo.navy_battle;
 import java.io.IOException;
 
 public class Launcher {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String args[]){
         int port = Integer.parseInt(args[0]);
-        if (args.length < 1) {
-            System.err.println("Arguments are missing!");
-        } else if (args.length == 1) {
-            Server server = new Server(port);
-            server.serverInit();
-        } else {
-            String addr = args[1];
-            ClientPOST clientServer = new ClientPOST(port);
-            clientServer.connexion(addr);
+        if (args.length > 0 ) {
+            try{
+                Server server = new Server(port);
+                server.serverInit();
+            }catch (Exception e) {}
+        }
+        if (args.length > 1 ) {
+            try{
+                String addr = args[1];
+                ClientPOST client_server = new ClientPOST(port);
+                client_server.connexion(addr);
+            }catch (Exception e) {}
         }
     }
 }
